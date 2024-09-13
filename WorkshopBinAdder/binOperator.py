d98b21349821a6db26d3c3442d569f30f4ebac3e
Julián Santiago Flórez Castañeda <jflorez33453@universidadean.edu.co>
2024-09-12 21:12:06 -0500

    binCalculator

--- WorkshopBinAdder/binOperator.py
+++ WorkshopBinAdder/binOperator.py
@@ -1,47 +1,65 @@
-def binaryAdd(bin1, bin2):
-    carry = False
-    sum = ""
+def AND(x,y):
+    return bool(x and y)
+
+def OR(x,y):
+    return bool(x or y)
+
+def NOT(x):
+    return bool(not x)
+
+def XOR(x,y):
+    return bool((x and not y) or (not x and y))
+
+def binAdd(a,b,c):
+    s = XOR(XOR(a, b), c)
+    c = OR(AND(a, b), AND(XOR(a, b), c))
+    return s, c
+
+def binSub(a,b,c):
+    s = XOR(XOR(a, b), c)
+    c = OR(AND(NOT(a), b), AND(XOR(NOT(a), b), c))
+    return s, c
+
+def binMul(bin1, bin2):
+    maxBin = max(len(bin1), len(bin2))
+    bin1 = bin1.zfill(maxBin)
+    bin2 = bin2.zfill(maxBin)
+    result = "0"
+    for i in range(maxBin):
+        if bin2[-i-1] == "1":
+            shifted_bin1 = bin1 + "0" * i
+            result = binOperation(result, shifted_bin1, binAdd)
+    return result
+
+def binOperation(bin1, bin2, operation):
+    if operation == binMul:
+        return binMul(bin1, bin2)
+    c = False
+    result = ""
+    neg = ""
+    greater = bin1
+    lower = bin2
     maxBin = max(len(bin1), len(bin2))
     bin1 = bin1.zfill(maxBin)
     bin2 = bin2.zfill(maxBin)
     for i in range(maxBin):
-        a = int(bin1[-i-1])
-        b = int(bin2[-i-1])
-        xor1 = bool((a and not b) or (not a and b))
-        cout1= bool(a and b)
-        xor2 = bool((xor1 and not carry) or (not xor1 and carry))
-        cout2 = bool(xor1 and carry)
-        or1 = bool(cout1 or cout2)
-        sum = str(int(xor2)) + sum
-        carry = or1
-    return str(int(carry)) + sum
+        if bin1[i] == "1" and bin2[i] == "0":
+            greater = bin1
+            lower = bin2
+            break
+        if bin1[i] == "0" and bin2[i] == "1":
+            greater = bin2
+            lower = bin1
+            if operation == binSub:
+                neg = "-"
+            break
+    for i in range(maxBin):
+        a = int(greater[-i-1])
+        b = int(lower[-i-1])
+        s, c = operation(a, b, c)
+        result = str(int(s)) + result
+    return neg + str(int(c)) + result
 
-def binarySubtraction(bin1, bin2):
-    carry = 0
-    result = ""
-    i = len(bin1) - 1
-    j = len(bin2) - 1
-    
-    while i >= 0 or j >= 0 or carry:
-        if i >= 0:
-            carry += int(bin1[i])
-            i -= 1
-        if j >= 0:
-            carry -= int(bin2[j])
-            j -= 1
-        result = str(carry % 2) + result
-        carry //= 2
-    
-    return result
-
-def binaryMultiplication(bin1, bin2):
-    result = "0"
-    for i in range(len(bin2)-1, -1, -1):
-        if bin2[i] == "1":
-            result = binaryAdd(result, bin1 + "0"*(len(bin2)-1-i))
-    
-    return result
-
-print("Binary Addition: ", binaryAdd("111100", "1001"))
-print("Binary Subtraction: ", binarySubtraction("111001011", "1100001"))
-print("Binary Multiplication: ", binaryMultiplication("1110", "1001"))
+print(binOperation("01010", "101000", binAdd))
+print(binOperation("11011001", "101010111", binSub))
+print(binOperation("1010", "1100", binMul))
